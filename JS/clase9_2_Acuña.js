923e91ce8d221148db2ae69e0a44805d37b01add
Franck-01 <87050434+Franck-01@users.noreply.github.com>
2021-10-11 20:39:53 -0300

    Delete clase4_Acuña.js

---
+++ JS/clase9_2_Acuña.js
@@ -0,0 +1,114 @@
+function misato() {
+	const modalCerrar = document.getElementById("modal-cerrar")
+	const modalAbrir = document.getElementById("modal-abrir")
+	const modalContainer = document.getElementsByClassName("misato")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function ritsuko() {
+	const modalCerrar = document.getElementById("modal-close")
+	const modalAbrir = document.getElementById("modal-open")
+	const modalContainer = document.getElementsByClassName("ritsuko")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function kaji() {
+	const modalCerrar = document.getElementById("kaji-close")
+	const modalAbrir = document.getElementById("kaji-open")
+	const modalContainer = document.getElementsByClassName("kaji")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function gendo() {
+	const modalCerrar = document.getElementById("gendo-close")
+	const modalAbrir = document.getElementById("gendo-open")
+	const modalContainer = document.getElementsByClassName("gendo")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function rei() {
+	const modalCerrar = document.getElementById("rei-close")
+	const modalAbrir = document.getElementById("rei-open")
+	const modalContainer = document.getElementsByClassName("rei")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function asuka() {
+	const modalCerrar = document.getElementById("asuka-close")
+	const modalAbrir = document.getElementById("asuka-open")
+	const modalContainer = document.getElementsByClassName("asuka")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function shinji() {
+	const modalCerrar = document.getElementById("shinji-close")
+	const modalAbrir = document.getElementById("shinji-open")
+	const modalContainer = document.getElementsByClassName("shinji")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function kaworu() {
+	const modalCerrar = document.getElementById("kaworu-close")
+	const modalAbrir = document.getElementById("kaworu-open")
+	const modalContainer = document.getElementsByClassName("kaworu")[0]
+
+	modalAbrir.addEventListener("click", () => {
+	modalContainer.classList.add("modal-active")
+	})
+	modalCerrar.addEventListener("click", () => {
+	modalContainer.classList.remove("modal-active")
+	})
+}
+function mari() {
+alert('la proxima piloto');
+
+const contenidoAdicional = document.getElementById("contAd");
+const adicional = document.createElement("div");
+
+const contenido = {
+	titulo: "Mari Illustrious Makinami",
+	item1: "Una chica con un gusto extraño por el LCL (liquido que usan los pilotos para sincronizacion con los Evas",
+	item2: "Y con una actitud muyexcentrica y ligeramente alocada",
+};
+adicional.innerHTML = `
+					<h1>${contenido.titulo}</h1>
+					<p>${contenido.item1}</p>
+					<p>${contenido.item2}</p>
+					`;
+contenidoAdicional.appendChild(adicional);
+}
